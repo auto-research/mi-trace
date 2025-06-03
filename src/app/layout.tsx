6780bc70a0f2077ad72339bf58f3b399707fb040
max <max.bian@ponyft.com>
2025-06-03 17:31:47 +0800

    feat: 在布局中添加 Google Analytics 追踪代码

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0G7RYZ8NK0"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0G7RYZ8NK0');
+          `,
+        }} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
